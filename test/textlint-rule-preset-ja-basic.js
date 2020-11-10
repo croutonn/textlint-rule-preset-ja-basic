afd455210a1dfd886dbd069ac601a71fb9c18821
croutonn <croutonna@gmail.com>
2020-11-10 22:54:24 +0900

    Add a editorconfig configuration file

--- test/textlint-rule-preset-ja-basic.js
+++ test/textlint-rule-preset-ja-basic.js
@@ -2,7 +2,8 @@
 "use strict";
 const assert = require("assert");
 const rules = require("../lib/textlint-rule-preset-ja-tech-loose").rules;
-const rulesConfig = require("../lib/textlint-rule-preset-ja-tech-loose").rulesConfig;
+const rulesConfig = require("../lib/textlint-rule-preset-ja-tech-loose")
+  .rulesConfig;
 describe("textlint-rule-preset-ja-tech-loose", function () {
   it("not missing key", function () {
     const ruleKeys = Object.keys(rules).sort();
