0d27e37ce09a669ba7c228829a32d8d5430d090a
croutonn <croutonna@gmail.com>
2021-05-29 00:49:12 +0900

    GitHub Packagesへ公開するためのワークフローを追加

--- test/textlint-rule-preset-ja-basic.js
+++ test/textlint-rule-preset-ja-basic.js
@@ -1,13 +1,13 @@
 // LICENSE : MIT
-"use strict";
-const assert = require("assert");
-const rules = require("../lib/textlint-rule-preset-ja-tech-loose").rules;
-const rulesConfig = require("../lib/textlint-rule-preset-ja-tech-loose")
-  .rulesConfig;
-describe("textlint-rule-preset-ja-tech-loose", function () {
-  it("not missing key", function () {
-    const ruleKeys = Object.keys(rules).sort();
-    const ruleConfigKeys = Object.keys(rulesConfig).sort();
-    assert.deepEqual(ruleKeys, ruleConfigKeys);
-  });
-});
+'use strict'
+const assert = require('assert')
+const rules = require('../lib/textlint-rule-preset-ja-tech-loose').rules
+const rulesConfig =
+  require('../lib/textlint-rule-preset-ja-tech-loose').rulesConfig
+describe('textlint-rule-preset-ja-tech-loose', function () {
+  it('not missing key', function () {
+    const ruleKeys = Object.keys(rules).sort()
+    const ruleConfigKeys = Object.keys(rulesConfig).sort()
+    assert.deepEqual(ruleKeys, ruleConfigKeys)
+  })
+})
